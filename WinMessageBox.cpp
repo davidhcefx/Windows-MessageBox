d922ac2edf732454f017b0be372108466c261a44
davidhcefx <davidhu0903ex3@gmail.com>
2019-10-02 04:14:55 +0800

    Update WinMessageBox.cpp

--- WinMessageBox.cpp
+++ WinMessageBox.cpp
@@ -8,15 +8,18 @@ using namespace std;
     Written by davidhcefx, 2019.9.27.
     Button: { OK: 0x0, OKCancel: 0x1, YesNo: 0x4, YesNoCancel: 0x3 }
     Icon: { Information: 0x40, None: 0x00, Question: 0x20, Stop: 0x10, Warning: 0x30 }
+    ReturnVal: { Fail: 0, OK: 1, Cancel: 2, Yes: 6, No: 7 }
+    Ref: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
 */
 
 int main(int argc, char* argv[])
 {
     if (argc < 3) {
-        cout << "Syntax: messagebox [Msg] [Title] (Button) (Icon)\n";
-        cout << "\tButton = ok | okcancel | yesno | yesnocancel (default=ok)\n";
-        cout << "\tIcon   = info | ques | stop | warn (default=info)\n";
-        cout << "\tNote that multibyte characters are not supported.\n";
+        cout << "Syntax: messagebox <Msg> <Title> (Button) (Icon)\n";
+        cout << "\tButton  =  ok | okcancel | yesno | yesnocancel (default=ok)\n";
+        cout << "\tIcon    =  info | ques | stop | warn (default=info)\n";
+        cout << "\t(Multi-byte characters are not supported)\n";
+        cout << "Return: 0 = fail, 1 = ok, 2 = cancel, 6 = yes, 7 = no.\n";
         return 0;
     } else {
         // Warning: This method cannot deal with multibyte characters.
